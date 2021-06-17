a7bd419a360362a2c0796f8aa606dc9cf9461b22
tushar276 <84825446+tushar276@users.noreply.github.com>
2021-06-16 21:13:54 -0500

    modified index.js all tasks are completed

--- index.js
+++ index.js
@@ -42,8 +42,32 @@ class Airplane {
   */
   
  class Person {
-    
+   constructor(name, age)
+   {
+     this.name = name;
+     this.age = age;
+     this.stomach = [];
+   }
+  
+    eat(somefood)
+    {
+      if (this.stomach.length < 10)
+      {
+         this.stomach.push(somefood);
+      }
+    }
+    poop()
+    {
+      this.stomach = [];
+    }
+    toString()
+    {
+      return(this.name + ", " + this.age);
+    }
   }
+  
+    
+
   
   /*
     TASK 2
@@ -58,11 +82,43 @@ class Airplane {
       - A car which runs out of `fuel` while driving can't drive any more distance:
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
-  
- class Car {
+  class Car {
+    constructor(model,milesPerGallon)
+    {
+      this.tank = 0;
+      this.odometer = 0;
+      this.model = model;
+      this.milesPerGallon = milesPerGallon;
+    }
+      fill(gallons)
+      {
+       this.tank = this.tank + gallons ;
+      }
+      drive(distance) {
+
+        // How many gallons do we need to drive this distance
+        var gallonsUsed = distance / this.milesPerGallon;
     
+        // Do we have enough gas in tank to drive the distance
+        if (gallonsUsed > this.tank) {
+          // We don't have enough gas so how far can we go before running out  
+          var drivableMiles = this.tank * this.milesPerGallon;
+          this.odometer += drivableMiles;
+    
+          // We should run out of gas so set tank to 0 and return a message
+          this.tank = 0;
+          return "I ran out of fuel at " + this.odometer + " miles!";
+        } 
+       
+        // We have enough gas to cover the distance, do normal drive functionality
+        this.odometer += distance;
+        this.tank -= gallonsUsed;
+      }
   }
+
   
+
+
   /*
     TASK 3
       - Write a Lambdasian class.
@@ -75,10 +131,19 @@ class Airplane {
           + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
           + {name} and {location} of course come from the instance's own properties.
   */
- class Lambdasian {
-    
+  class Lambdasian {
+    constructor(data)
+    {
+    this.name = data.name;
+    this.age = data.age;
+    this.location = data.location;
   }
-  
+    speak()
+    {
+      return('Hello my name is ' + this.name + ", I am from " + this.location);
+    }
+  }
+   
   /*
     TASK 4
       - Write an Instructor class extending Lambdasian.
@@ -93,8 +158,24 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+   class Instructor extends Lambdasian {
+   constructor(data) 
+  {
+    super(data);
+    this.specialty = data.specialty;
+    this.favLanguage = data.favLanguage;
+    this.catchPhrase = data.catchPhrase;
+ }
+    demo(subject)
+    {
+      return('Today we are learning about' + subject );
+    }
+    grade(student, subject) 
+    {
+      return(student.name + " receives a perfect score on " + subject);
+    }
 
+    
  }
   /*
     TASK 5
@@ -111,8 +192,26 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
-     
+ class Student extends Lambdasian {
+   constructor(data){
+     super(data);
+     this.previousBackground = data.previousBackground;
+     this.className = data.className;
+     this.favSubjects = data.favSubjects;
+   }
+    listSubjects()
+    {
+      return(this.favSubjects.toString());
+    }
+
+    PRAssignment(subject)
+    {
+     return(this.name + " has submitted a PR for " + subject);
+    }
+    sprintChallenge(subject)
+    {
+    return(this.name + " has begun sprint challenge on " + subject );
+    }
  }
   
   /*
@@ -128,8 +227,24 @@ class Airplane {
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
- class ProjectManager {
-     
+ class ProjectManager extends Instructor {
+  constructor(data){
+    super(data);
+    this.gradClassName = data.gradClassName;
+    this.favInstructor = data.favInstructor;
+    
+  }
+   standUp(channel)
+   {
+     return(this.name + " announces to " + channel + " , @channel standy times!");
+   }
+
+   debugsCode(student,subject)
+   {
+    return(this.name + " debugs " + student.name + " 's code on " + subject);
+   }
+   
+   
  }
   /*
     STRETCH PROBLEM (no tests!)
